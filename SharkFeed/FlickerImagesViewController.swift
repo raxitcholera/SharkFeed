1dc3125cdf609a647a5c02b6d1ca9f3995e86c4b
raxitcholera <raxitcholera@gmail.com>
2018-02-12 19:46:55 -0800

    Added Collection view in the view with Autolayout Constraints and created Constants file with respect to Flicker client.

---
+++ SharkFeed/FlickerImagesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FlickerImagesViewController.swift
+//  SharkFeed
+//
+//  Created by Raxit Cholera on 2/12/18.
+//  Copyright © 2018 Raxit Cholera. All rights reserved.
+//
+
+import UIKit
+
+class FlickerImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+extension FlickerImagesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
